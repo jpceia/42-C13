e18d3908d21c07f250fa871716dc1290b3f924c8
jpceia <joao.p.ceia@gmail.com>
2021-11-15 23:22:41 +0000

    ex01

---
+++ ex01/btree_apply_prefix.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_prefix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 23:16:34 by jpceia            #+#    #+#             */
+/*   Updated: 2021/11/15 23:20:16 by jpceia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void btree_apply_prefix(t_btree *root, void (*applyf)(void *))
+{
+	if (!root)
+		return ;
+	applyf(root->item);
+	btree_apply_prefix(root->left, applyf);
+	btree_apply_prefix(root->right, applyf);
+}
