ea6f8b8edfe41b1fe08db774177938c7c1d80618
jpceia <joao.p.ceia@gmail.com>
2021-11-15 23:42:33 +0000

    ex03

---
+++ ex03/btree_apply_suffix.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_suffix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 23:25:52 by jpceia            #+#    #+#             */
+/*   Updated: 2021/11/15 23:41:28 by jpceia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	btree_apply_suffix(t_btree *root, void (*applyf)(void *))
+{
+	if (!root)
+		return ;
+	btree_apply_suffix(root->left, applyf);
+	btree_apply_suffix(root->right, applyf);
+	applyf(root->item);
+}
