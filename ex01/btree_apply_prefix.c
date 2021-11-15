a1b100d81bdcd47a964f14f7e406cfebf890f7fa
jpceia <joao.p.ceia@gmail.com>
2021-11-15 23:42:54 +0000

    norm fixes

--- ex01/btree_apply_prefix.c
+++ ex01/btree_apply_prefix.c
@@ -6,13 +6,13 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 23:16:34 by jpceia            #+#    #+#             */
-/*   Updated: 2021/11/15 23:20:16 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/11/15 23:41:14 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_btree.h"
 
-void btree_apply_prefix(t_btree *root, void (*applyf)(void *))
+void	btree_apply_prefix(t_btree *root, void (*applyf)(void *))
 {
 	if (!root)
 		return ;
--- ex02/btree_apply_infix.c
+++ ex02/btree_apply_infix.c
@@ -6,17 +6,17 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 23:24:33 by jpceia            #+#    #+#             */
-/*   Updated: 2021/11/15 23:36:33 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/11/15 23:41:24 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_btree.h"
 
-void btree_apply_infix(t_btree *root, void (*applyf)(void *))
+void	btree_apply_infix(t_btree *root, void (*applyf)(void *))
 {
-    if (!root)
-        return ;
+	if (!root)
+		return ;
 	btree_apply_infix(root->left, applyf);
-    applyf(root->item);
-    btree_apply_infix(root->right, applyf);
+	applyf(root->item);
+	btree_apply_infix(root->right, applyf);
 }
